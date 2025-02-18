e6f37a0ab376e227231a5f86b5a67912816e896d
Demasse <demassedimas2006@gmail.com>
2025-02-18 10:32:54 +0100

    nav-bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,24 @@
 import React from "react";
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import { BsClipboardData, BsBriefcase, BsChatSquare } from 'react-icons/bs';
+import { Link } from 'react-scroll'
 
-import{B1HomeAlt, B1User } from 'react-icons/b1';
-import{BsclipboardData, BsBriefcase , BsChatsquare} from 'react-icons/bs';
-
-import { Link} from 'react-scroll';
 const Nav = () => {
-return <nav>
-    
-    <div className="container mx-auto">
+    return (
+        <nav className=" fixed bottom-2 lg:bottom-8 w-full overflow-hidden  z-50 ">
+            <div className="container mx-auto">
+                <div className=" w-full bg-black/20 h-[96px] backdrop-blur-2xl ">
+                    <Link>
+                        <BiHomeAlt />
 
-     <div className=""> 
-     <Link>
-     <B1HomeAlt /> 
-     </Link>
-     </div>
+                    </Link>
 
-    </div>
+                </div>
 
+            </div>
 
-    </nav>
-
+        </nav>
+    );
 }
 
 export default Nav;
