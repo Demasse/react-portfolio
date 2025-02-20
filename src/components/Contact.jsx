e9e7ff2030efba3139522e84c59efed29a6ecb80
Demasse <demassedimas2006@gmail.com>
2025-02-20 23:17:18 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-return <div className=" section" id="contact"  >contact</div>
+return (
+  <div className=" section" id="contact"  >
+
+    contact
+    
+    </div>
+ ) 
 
 }
 
